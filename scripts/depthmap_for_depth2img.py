3dfaf26fd41770ecdfed39e84557c025052cc730
VictorV <vv474172261@hotmail.com>
2023-04-05 14:43:25 +0800

    Update depthmap_for_depth2img.py

--- scripts/depthmap_for_depth2img.py
+++ scripts/depthmap_for_depth2img.py
@@ -6,26 +6,41 @@ import contextlib
 from PIL import Image
 from modules.shared import opts, cmd_opts
 from modules import processing, images, shared, devices
+import os
 
 from torchvision.transforms import Compose
-from repositories.midas.midas.dpt_depth import DPTDepthModel
-from repositories.midas.midas.midas_net import MidasNet
-from repositories.midas.midas.midas_net_custom import MidasNet_small
-from repositories.midas.midas.transforms import Resize, NormalizeImage, PrepareForNet
+from repositories.midas.midas.model_loader import load_model
 
 import numpy as np
 
 class SimpleDepthMapGenerator(object):
-    def calculate_depth_maps(self,image,img_x,img_y,model_type,invert_depth):
+    def calculate_depth_maps(self,image,img_x,img_y,model_type_index,invert_depth):
         try:
+            model = None
             def download_file(filename, url):
-                print("Downloading midas model weights to %s" % filename)
-                with open(filename, 'wb') as fout:
-                    response = requests.get(url, stream=True)
-                    response.raise_for_status()
-                    # Write response data to file
-                    for block in response.iter_content(4096):
-                        fout.write(block)
+                print(f"download {filename} form {url}")
+                import sys
+                try:
+                    with open(filename+'.tmp', "wb") as f:
+                        response = requests.get(url, stream=True)
+                        total_length = response.headers.get('content-length')
+
+                        if total_length is None: # no content length header
+                            f.write(response.content)
+                        else:
+                            dl = 0
+                            total_length = int(total_length)
+                            for data in response.iter_content(chunk_size=4096):
+                                dl += len(data)
+                                f.write(data)
+                                done = int(50 * dl / total_length)
+                                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
+                                sys.stdout.flush()
+                    os.rename(filename+'.tmp', filename)
+                except Exception as e:
+                    os.remove(filename+'.tmp')
+                    print("\n--------download fail------------\n")
+                    raise e
 
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
             # model path and name
@@ -34,86 +49,30 @@ class SimpleDepthMapGenerator(object):
             os.makedirs(model_dir, exist_ok=True)
             print("Loading midas model weights ..")
             device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-            #"dpt_large"
-            if model_type == 0:
-                model_path = f"{model_dir}/dpt_large-midas-2f21e586.pt"
-                print(model_path)
-                if not os.path.exists(model_path):
-                    download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_large-midas-2f21e586.pt")
-                model = DPTDepthModel(
-                    path=model_path,
-                    backbone="vitl16_384",
-                    non_negative=True,
-                )
-                net_w, net_h = 384, 384
-                resize_mode = "minimal"
-                normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-
-            #"dpt_hybrid" / not working at the moment
-            # elif model_type == 1:
-            #     model_path = f"{model_dir}/dpt_hybrid-midas-501f0c75.pt"
-            #     print(model_path)
-            #     if not os.path.exists(model_path):
-            #         download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt")
-            #     model = DPTDepthModel(
-            #         path=model_path,
-            #         backbone="vitb_rn50_384",
-            #         non_negative=True,
-            #     )
-            #     net_w, net_h = 384, 384
-            #     resize_mode="minimal"
-            #     normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
-
-            #"midas_v21"
-            elif model_type == 1:
-                model_path = f"{model_dir}/midas_v21-f6b98070.pt"
-                print(model_path)
-                if not os.path.exists(model_path):
-                    download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt")
-                model = MidasNet(model_path, non_negative=True)
-                net_w, net_h = 384, 384
-                resize_mode="upper_bound"
-                normalization = NormalizeImage(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                )
-
-            #"midas_v21_small"
-            elif model_type == 2:
-                model_path = f"{model_dir}/midas_v21_small-70d6b9c8.pt"
-                print(model_path)
-                if not os.path.exists(model_path):
-                    download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21_small-70d6b9c8.pt")
-                model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=True, non_negative=True, blocks={'expand': True})
-                net_w, net_h = 256, 256
-                resize_mode="upper_bound"
-                normalization = NormalizeImage(
-                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-                )
-
-            # init transform
-            transform = Compose(
-                [
-                    Resize(
-                        img_x,
-                        img_y,
-                        resize_target=None,
-                        keep_aspect_ratio=True,
-                        ensure_multiple_of=32,
-                        resize_method=resize_mode,
-                        image_interpolation_method=cv2.INTER_CUBIC,
-                    ),
-                    normalization,
-                    PrepareForNet(),
-                ]
-            )
-            model.eval()
-            # optimize
-            if device == torch.device("cuda"):
-                model = model.to(memory_format=torch.channels_last)
-                if not cmd_opts.no_half:
-                    model = model.half()
-            model.to(device)
+            models = ["dpt_beit_large_512",
+                "dpt_beit_large_384",
+                "dpt_beit_base_384",
+                "dpt_swin2_large_384",
+                "dpt_swin2_base_384",
+                "dpt_swin2_tiny_256",
+                "dpt_swin_large_384",
+                "dpt_next_vit_large_384",
+                "dpt_levit_224",
+                "dpt_large_384",
+                "dpt_hybrid_384",
+                "midas_v21_384",
+                "midas_v21_small_256",
+                # "openvino_midas_v21_small_256"
+            ]
+            model_path = model_dir + '/' + models[model_type_index] + '.pt'
+            if not os.path.exists(model_path):
+                if models.index("midas_v21_384") <= model_type_index:
+                    download_file(model_path, "https://github.com/isl-org/MiDaS/releases/download/v2_1/"+ models[model_type_index] + ".pt")
+                elif models.index("midas_v21_384") > model_type_index > models.index("dpt_large_384"):
+                    download_file(model_path, "https://github.com/isl-org/MiDaS/releases/download/v3/"+ models[model_type_index] + ".pt")
+                else:
+                    download_file(model_path, "https://github.com/isl-org/MiDaS/releases/download/v3_1/"+ models[model_type_index] + ".pt")
+            model, transform, net_w, net_h = load_model(device, model_path, models[model_type_index], (img_x, img_y))
 
             img = cv2.cvtColor(np.asarray(image), cv2.COLOR_BGR2RGB) / 255.0
             img_input = transform({"image": img})["image"]
